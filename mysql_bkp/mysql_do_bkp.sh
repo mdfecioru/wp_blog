dca114ee57f0266810a68b8d88f8b29549981232
EC2 Default User <ec2-user@ip-172-31-26-72.eu-west-1.compute.internal>
2016-08-21 11:08:20 +0000

    New version

--- mysql_bkp/mysql_do_bkp.sh
+++ mysql_bkp/mysql_do_bkp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysqldump --add-drop-table -u root -p${MYSQL_ROOT_PASSWORD} wordpress > /mysql_bkp/blog1.bak.sql
+mysqldump --add-drop-table -u root -p${MYSQL_ROOT_PASSWORD} wordpress > /mysql_bkp/blog`date +%d%b%Y`.bak.sql
 
